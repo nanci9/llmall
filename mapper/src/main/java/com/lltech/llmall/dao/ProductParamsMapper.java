550f3f1a82b2b276c913260ab43396d1031576df
linmu <979529235@qq.com>
2023-11-24 09:51:32 +0800

    Changes

--- mapper/src/main/java/com/lltech/llmall/dao/ProductParamsMapper.java
+++ mapper/src/main/java/com/lltech/llmall/dao/ProductParamsMapper.java
@@ -2,6 +2,8 @@ package com.lltech.llmall.dao;
 
 import com.lltech.llmall.entity.ProductParams;
 import com.lltech.llmall.general.GeneralDAO;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductParamsMapper extends GeneralDAO<ProductParams> {
 }
